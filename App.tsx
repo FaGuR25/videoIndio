e8e2d17780b07d3ea75e2e5b36ce08271164cf6f
Fatima <guadalupe20050@gmail.com>
2024-05-29 14:38:06 -0600

    notas

--- App.tsx
+++ App.tsx
@@ -1,4 +1,81 @@
-/* import React from 'react';
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Icon} from 'react-native-elements';
+
+import Login from './screens/Login';
+import HomeScreen from './screens/HomeScreen';
+import FindScreen from './screens/FindScreen';
+import ChatScreen from './screens/ChatScreen';
+import PostScreen from './screens/PostScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator initialRouteName="Inicio">
+      <Tab.Screen
+        name="Inicio"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: props => (
+            <Icon type="feather" name="home" color={props.color} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Find" component={FindScreen} />
+      <Tab.Screen
+        name="notas"
+        component={ChatScreen}
+        options={{
+          title: 'notas',
+          headerTitleAlign: 'center',
+          headerStyle: {backgroundColor: '#881874'},
+          headerTintColor: 'white',
+        }}
+      />
+      <Tab.Screen name="Post" component={PostScreen} />
+      <Tab.Screen name="Setting" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Tabs"
+          component={TabNavigator}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+/*
+
+options={{
+          title: 'notas',
+          headerTitleAlign: 'center',
+          headerStyle: {backgroundColor: '#881874'},
+          headerTintColor: 'white',
+        }}
+
+
+
+import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {Icon} from 'react-native-elements';
@@ -79,61 +156,6 @@ export default function App() {
 
 
 */
-
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Icon} from 'react-native-elements';
-
-import Login from './screens/Login';
-import HomeScreen from './screens/HomeScreen';
-import FindScreen from './screens/FindScreen';
-import ChatScreen from './screens/ChatScreen';
-import PostScreen from './screens/PostScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import {createStackNavigator} from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator initialRouteName="Inicio">
-      <Tab.Screen
-        name="Inicio"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: props => (
-            <Icon type="feather" name="home" color={props.color} />
-          ),
-        }}
-      />
-      <Tab.Screen name="Find" component={FindScreen} />
-      <Tab.Screen name="Chat" component={ChatScreen} />
-      <Tab.Screen name="Post" component={PostScreen} />
-      <Tab.Screen name="Setting" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-}
-
-export default function App(): React.JSX.Element {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Tabs"
-          component={TabNavigator}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
 
 /*
 return (
--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,22 +1,80 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 
-const ChatScreen = ({navigation}) => {
+import CreateNotes from './CreateNotes';
+import DetailsNote from './DetailsNote';
+import {Touchable} from 'react-native';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text> Chat Screen </Text>
-      <Button title="Click here" onPress={() => alert('Button Clicked!')} />
-    </View>
-  );
-};
+    <Stack.Navigator>
+      <Stack.Screen
+        name="crear"
+        component={CreateNotes}
+        options={{
+          title: 'CREAR NOTAS',
+          headerTitleAlign: 'center',
+          headerStyle: {backgroundColor: '#8d1874'},
+          headerTintColor: 'white',
+        }}
+      />
 
-export default ChatScreen;
+      <Stack.Screen
+        name="Detail"
+        component={DetailsNote}
+        options={{
+          title: 'NOTAS',
+          headerTitleAlign: 'center',
+          headerStyle: {backgroundColor: '#8d1874'},
+          headerTintColor: 'white',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function ChatScreen(props): React.JSX.Element {
+  return (
+    <>
+      <ScrollView>
+        <View>
+          <TouchableOpacity
+            style={styles.boton}
+            onPress={() => props.navigation.navigate('crear')}>
+            <Text style={styles.textoBoton}>Agregar una nueva nota</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
+      <MyStack />
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '8fcbbc',
+  boton: {
+    backgroundColor: '#b71375',
+    borderColor: '#fc4f00',
+    borderRadius: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+  },
+  textoBoton: {
+    textAlign: 'center',
+    padding: 10,
+    color: 'white',
+    fontSize: 16,
   },
 });
