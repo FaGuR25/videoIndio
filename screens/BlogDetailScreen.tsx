e84d687306e55a8562e562d8562b47306523f894
Fatima <guadalupe20050@gmail.com>
2024-07-18 20:48:07 -0600

    estilos en los blogs

--- screens/BlogDetailScreen.tsx
+++ screens/BlogDetailScreen.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
 
-const BlogDetailScreen = ({ route }) => {
-  const { blog } = route.params;
+const BlogDetailScreen = ({route}) => {
+  const {blog} = route.params;
 
   return (
     <ScrollView style={styles.container}>
       <Image source={blog.image} style={styles.image} />
       <Text style={styles.title}>{blog.title}</Text>
       <Text style={styles.content}>{blog.content}</Text>
-      <Text style={styles.content}>{blog.footer}</Text>
+      {blog.footer && <Text style={styles.footer}>{blog.footer}</Text>}
     </ScrollView>
   );
 };
@@ -18,21 +18,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#8fcbbc',
   },
   image: {
     width: '100%',
     height: 200,
-    borderRadius: 8,
-    marginBottom: 16,
+    borderRadius: 20,
+    marginBottom: 8,
+    backgroundColor: '#ecffec',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 8,
+    color: 'black',
+    textAlign: 'center',
   },
   content: {
     fontSize: 16,
+    color: 'black',
+    marginBottom: 8,
+    lineHeight: 24,
+    textAlign: 'justify',
+  },
+  footer: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 20,
+    fontStyle: 'italic',
   },
 });
 
